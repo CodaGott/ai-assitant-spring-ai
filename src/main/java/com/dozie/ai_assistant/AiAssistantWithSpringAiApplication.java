9d462486ec981d0c6b0df0452a60c9c7e47c28dc
Chukwuebuka Uyanwune <dozie@golivwell.com>
2025-02-19 23:45:15 +0100

    initial commit

---
+++ src/main/java/com/dozie/ai_assistant/AiAssistantWithSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.dozie.ai_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAssistantWithSpringAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiAssistantWithSpringAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dozie/ai_assistant/AiAssistantWithSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dozie.ai_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAssistantWithSpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
