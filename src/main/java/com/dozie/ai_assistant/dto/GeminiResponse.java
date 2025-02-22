9c5145ef3f5e4c22d94936dc4647b231e8068a5a
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2025-02-22 18:11:01 +0100

    gemini api response

---
+++ src/main/java/com/dozie/ai_assistant/dto/GeminiResponse.java
@@ -0,0 +1,32 @@
+package com.dozie.ai_assistant.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GeminiResponse {
+    private List<Candidate> candidates;
+
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @Data
+    public static class Candidate {
+        private Content content;
+
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @Data
+    public static class Content {
+        private List<Part> parts;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @Data
+    public static class Part {
+        private String text;
+    }
+}
