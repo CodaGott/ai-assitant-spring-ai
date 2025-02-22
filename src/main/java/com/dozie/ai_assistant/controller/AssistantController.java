3a77095ff83de22988af645ba5aecfc1b0c48384
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2025-02-22 18:12:12 +0100

    controller update

--- src/main/java/com/dozie/ai_assistant/controller/AssistantController.java
+++ src/main/java/com/dozie/ai_assistant/controller/AssistantController.java
@@ -3,15 +3,10 @@ package com.dozie.ai_assistant.controller;
 import com.dozie.ai_assistant.dto.AssistantRequest;
 import com.dozie.ai_assistant.service.AssistantService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/research")
+@RequestMapping("/api/v1/research")
 @RestController
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
